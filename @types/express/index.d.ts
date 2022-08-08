aad595a8ac88875b0305618a4eb829f8689afabf
Elvis Onobo <elvis.onobo@gmail.com>
2022-08-08 15:18:38 +0100

    Fix: fix express types error for auth middleware

---
+++ @types/express/index.d.ts
@@ -0,0 +1,10 @@
+import { Request } from "express";
+import jsonwebtoken, {JwtPayload} from 'jsonwebtoken'
+
+declare global {
+  namespace Express {
+    export interface Request {
+        user: any
+    }
+  }
+}
